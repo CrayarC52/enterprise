5bdb9074d00974c03a277b0a49ad28dc9902db26
Logan Conley <logan.conley123@gmail.com>
2023-09-05 15:15:00 -0400

    First Commit of enterprise

--- src/main/java/com/myplantdiary/enterprise/EnterpriseApplication.java
+++ src/main/java/com/myplantdiary/enterprise/EnterpriseApplication.java
@@ -2,7 +2,7 @@ package com.myplantdiary.enterprise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//Hello?
+//Hello? hello
 @SpringBootApplication
 public class EnterpriseApplication {
 
